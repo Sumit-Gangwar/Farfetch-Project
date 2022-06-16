bacae4b4c30cc9dcca8798668c558967e0ea49fb
Vishall001 <vshmandloi@gmail.com>
2022-06-16 14:37:31 +0530

    Final

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,10 +1,11 @@
 let btn = document.getElementById("placed").addEventListener("click",function(){
     let first = document.getElementById("first").value
     let last = document.getElementById("last").value
-    // let address =document.querySelectorAll("address")
-
-    let city = document.getElementById("city")
-    if(first !="" && last!="" && city!=""){
+    // let address =document.getElementsByClassName(".address").value
+    let state= document.getElementById("state").value
+    let city = document.getElementById("city").value
+    let phone = document.getElementById("phone").value
+    if(first !="" && last!="" && city!="" && state != "" && city != "" && phone !=""){
        
         alert ("Order Placed")
    }
